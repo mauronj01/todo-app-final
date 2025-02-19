5d48354d00eeee83615ce192c52008443476e2c7
mauronj01 <julienmauron@bluewin.ch>
2025-02-19 14:15:03 +0100

    push2

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://containerapp-mauronjulien-300722.wittyforest-6b34e2a0.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
